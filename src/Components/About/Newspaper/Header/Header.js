f2ecdc7ea4c4a7caba263034d5dc2f1892369193
Bertrand Bourion <bertrandbou@hotmail.fr>
2021-01-20 23:40:29 +0100

    feat(newspaper): display today's date

--- src/Components/About/Newspaper/Header/Header.js
+++ src/Components/About/Newspaper/Header/Header.js
@@ -1,28 +1,21 @@
 import "./Header.css";
 import Paparazzi from "../../../../assets/img/paparazzi.png";
 import LimitedEdition from "../../../../assets/img/limited-edition.png";
+import { getCurrentDate } from "../../../../utils/getCurrentDate";
 
 const Header = () => {
   return (
     <div id="title-container" className="flex flex-col mt-4">
       <div className="flex justify-between">
-        <img
-          src={Paparazzi}
-          alt="Paparazzi"
-          className="logo mt-6"
-        />
+        <img src={Paparazzi} alt="Paparazzi" className="logo mt-6" />
         <p id="title" className="text-center mt-2 -ml-8 louisa-cp">
           The Story
         </p>
-        <img
-          src={LimitedEdition}
-          alt="Paparazzi"
-          className="logo mt-8"
-        />
+        <img src={LimitedEdition} alt="Paparazzi" className="logo mt-8" />
       </div>
       <div className="flex justify-around text-2xl border-black border-t-2 border-b-2 old-newspaper -mt-2">
         <span>6201Z</span>
-        <span className="ml-2">Mercredi 20 janvier 2021</span>
+        <span className="ml-2">{getCurrentDate()}</span>
         <span>N°1</span>
       </div>
     </div>
